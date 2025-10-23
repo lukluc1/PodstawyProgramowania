b1e4e4d0f4816faba930be9bbefcca1fbbf83ff7
1C gr 2 <lukasz.luc.2020@gmail.com>
2025-10-23 12:36:24 +0200

    instrukcje warunkowe zadania

---
+++ instrukcje_warunkowe_zadania.py
@@ -0,0 +1,23 @@
+#rozwiązywanie równania kwadratowego
+
+'''a = float(input('Podaj liczbe a =/= 0'))
+b = float(input('Podaj liczbe b'))
+c = float(input('Podaj liczbe c'))
+delta = b ** 2 - 4 * a * c
+
+if delta > 0:
+    x1 = (-b - delta ** 0.5) / (2 * a)
+    x2 = (-b + delta ** 0.5) / (2 * a)
+    print(f'x1 = {x1} v x2 = {x2}')
+elif delta == 0:
+    x = (-b) / (2 * a)
+    print('x1 = x2 = {}'.format(x))
+else:
+    print('brak rozwiązań')'''
+
+#zadanie 12
+pisemny_j_poski = int(input('pisemny polski'))
+pisemny_j_obcy = int(input('pisemny obcy'))
+pisemny_j_dodatkowy = int(input('pisemny dodatkowy'))
+ustny_j_poski = int(input('ustny polski'))
+ustny_j_obcy = int(input('ustny obcy'))
