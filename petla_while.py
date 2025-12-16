f352ba8f44a4decb594567b272096f32aa1dfe18
1C gr 2 <lukasz.luc.2020@gmail.com>
2025-12-16 13:43:22 +0100

    gra hazardowa

--- petla_while.py
+++ petla_while.py
@@ -21,7 +21,7 @@ while x != 'q':
 print(licznik)'''
 
 #zad 2
-popr_haslo = 'informatyka'
+'''popr_haslo = 'informatyka'
 haslo = input('podaj haslo')
 proba = 1
 while haslo != popr_haslo and proba < 5:
@@ -32,4 +32,42 @@ while haslo != popr_haslo and proba < 5:
 if haslo == popr_haslo:
     print('witaj w systemie')
 else:
-    print('nie ma hasla nie ma dostepu')
+    print('nie ma hasla nie ma dostepu')'''
+
+#zad 6
+'''wynik1 = 0
+wynik2 = 0
+akcja = 0
+while not ((wynik1 >= 21 or wynik2 >= 21) and abs(wynik1 - wynik2) >= 2): #abs(x) = |x|
+    akcja += 1  # akcja = akcja + 1
+    print(f'akcja {akcja}')
+    druzyna = int(input('Podaj numer druzyny, ktora wygrala akcje'))
+    if druzyna == 1:
+        wynik1 += 1
+    elif druzyna == 2:
+        wynik2 += 1
+    print(f'Wynik {wynik1} : {wynik2}')
+if wynik1 > wynik2:
+    print('wygrala druzyna 1')
+else:
+    print('wygrala druzyna 2')'''
+#zad 6 random
+from random import randint
+from time import sleep
+wynik1 = 0
+wynik2 = 0
+akcja = 0
+while not ((wynik1 >= 21 or wynik2 >= 21) and abs(wynik1 - wynik2) >= 2): #abs(x) = |x|
+    akcja += 1  # akcja = akcja + 1
+    print(f'akcja {akcja}')
+    druzyna = randint(1, 2)
+    if druzyna == 1:
+        wynik1 += 1
+    elif druzyna == 2:
+        wynik2 += 1
+    print(f'Wynik {wynik1} : {wynik2}')
+    sleep(0.5)
+if wynik1 > wynik2:
+    print('wygrala druzyna 1')
+else:
+    print('wygrala druzyna 2')
