b30c422b815c8e0af928679ca91dad9ebcbf6031
1C gr 2 <lukasz.luc.2020@gmail.com>
2025-12-16 13:55:07 +0100

    hazard 2 cz 2

--- hazard.py
+++ hazard.py
@@ -2,18 +2,23 @@ from random import randint
 from time import sleep
 wynik1 = 0
 wynik2 = 0
+wynik3 = 0
 akcja = 0
-while not ((wynik1 >= 21 or wynik2 >= 21) and abs(wynik1 - wynik2) >= 2): #abs(x) = |x|
+while not ((wynik1 >= 5 or wynik2 >= 5 or wynik3 >= 5) and abs(wynik1 - wynik2 or wynik3 - wynik2 or wynik3 - wynik1) >= 2): #abs(x) = |x|
     akcja += 1  # akcja = akcja + 1
     print(f'akcja {akcja}')
-    druzyna = randint(1, 2)
+    druzyna = randint(1, 3)
     if druzyna == 1:
         wynik1 += 1
     elif druzyna == 2:
         wynik2 += 1
-    print(f'Wynik {wynik1} : {wynik2}')
-    sleep(0.5)
-if wynik1 > wynik2:
+    else:
+        wynik3 += 1
+    print(f'Wynik {wynik1} : {wynik2} : {wynik3}')
+    sleep(0.25)
+if wynik1 > wynik2 and wynik1 > wynik3:
     print('wygrala druzyna 1')
-else:
+elif wynik2 > wynik1 and wynik2 > wynik3:
     print('wygrala druzyna 2')
+elif wynik3 > wynik1 and wynik3 > wynik2:
+    print('wygrala druzyna 3')
