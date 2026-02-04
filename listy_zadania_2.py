eb2df803ff1313c0e71474b420cfd09b7837ab35
Lukasz <lukasz.luc.2020@gmail.com>
2026-02-04 10:34:43 +0100

    listy zadania cd

--- listy_zadania_2.py
+++ listy_zadania_2.py
@@ -11,3 +11,69 @@ print(lista_norm)'''
 lista = [123, 89, 5600, 432, 11, 45, 900, 12450, 1410, 390, 9999]
 '''lista = [x for x in lista if x < 1000 or x > 9999]'''
 lista = [x for x in lista if not (x >= 1000 and x <=9999)]
+
+
+#zad4
+tekst = 'LITWOOJCZYZNOMOJATYJESTESZJAKZDROWIEILECIETRZEBACENICTENTYLKOSIEDOWIEKTOCIESTRACILDZISPIEKNOSCTWACALEJOZDOBIEWIDZEIOPISUJEBOTESKNIEPOTOBIEPANNOSWIETACOJASNEJBRONISZCZESTOCHOWYIOSTREJSWIECISZBRAMIETYCOGRODZAMKOWYNOWOGRODZKIOCHRANIASZZJEGOWIERNMLUDEMJAKMNIEDZIECKODOZDROWIAPOWROCILASCUDYMGDYODPLACZACEJMATKIPODTWOJAOPIEKEOFIAROWANYMARTWAPODNIOSELMOJEPOWIEKEIZARAZMOGLEMPIESZODOTWYCZSWIATYNPROGUISTZAWROCONEZYCIEPODZIEKOWACBOGUTAKNASPOWROCISCUDYMNADOJCZYZNYLONOTYMCZASEMPRENOSMOJADUSZEUTESKNIONADOTYCHPAGORKOWLESNYCHDOTYCHLAKZIELONYCHSZEROKONADBLEKITNYMNIEMNEMROZCIAGNIONYCHDOTYCHPOLMALOWANYCHZBOZEMROZMAITEMWYZLACANYCHPSZENICAPOSREBRZANYCHZYTEMGDZIEBURSZTYNOWYSWIERZOPGRYKAJAKSNIEGBIALAGDZIEPANIENSKIMRUMIENCEMDZIECIELINAPALAAWSZYSTKOPRZEPASANEJAKBYWSTEGAMIEDZAZIELONANANIJZZADKACICHEGRUSZESIEDZA'
+
+suma = 0
+for x in tekst:
+    suma += ord(x)
+
+print(suma)
+
+#sposob 2
+print(sum(list(map(ord, tekst))))
+'''kody_ascii_map = map(ord, tekst)
+kody_ascii_map = list(kody_ascii_map)
+print(sum(kody_ascii_map))'''
+
+#zad5
+plansza = [
+[3, 8, 1, 9],
+[4, 6, 5, 2],
+[7, 1, 8, 3],
+[2, 9, 4, 6]
+]
+'''#średnie w wierszach
+for x in plansza:
+    print(sum(x) / len(x))
+
+#średnie w kolumnach
+for i in range(len(plansza[0])):
+    suma = 0
+    for j in range(len(plansza)):
+        suma += plansza[j][i]
+    print(suma / len(plansza))'''
+
+#zad 6
+for i in range(len(plansza)):
+    for j in range(len(plansza[0])):
+        licznik = 0
+        srodkowy = plansza[i][j] #szary element
+        '''if srodkowy > plansza [i - 1][j]:
+            licznik += 1
+        if srodkowy < plansza [i + 1][j +1]:
+            licznik += 1'''
+
+        for x in range(i - 1, (i + 1) + 1):
+            for y in range(j - 1, (j + 1) + 1):
+                if x < 0:
+                    x = len(plansza) - 1
+                elif x > len(plansza) - 1:
+                    x = 0
+                if y < 0:
+                    y = len(plansza[0]) - 1
+                elif y > len(plansza[0]) - 1:
+                    y = 0
+                czerwony = plansza[x][y]
+                if srodkowy < czerwony:
+                    licznik += 1
+            print(i, j, licznik)
+
+
+
+
+
+
+
