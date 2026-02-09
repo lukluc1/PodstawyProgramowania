59eff84aec486cd0cd5a8b296186a652dfbff609
1C gr 2 <lukasz.luc.2020@gmail.com>
2026-02-09 11:24:26 +0100

    zbiory

--- listy_zadania_2.py
+++ listy_zadania_2.py
@@ -28,13 +28,14 @@ print(sum(list(map(ord, tekst))))
 kody_ascii_map = list(kody_ascii_map)
 print(sum(kody_ascii_map))'''
 
-#zad5
+#zad6
 plansza = [
 [3, 8, 1, 9],
 [4, 6, 5, 2],
 [7, 1, 8, 3],
 [2, 9, 4, 6]
 ]
+#6.1
 '''#średnie w wierszach
 for x in plansza:
     print(sum(x) / len(x))
@@ -46,19 +47,20 @@ for i in range(len(plansza[0])):
         suma += plansza[j][i]
     print(suma / len(plansza))'''
 
-#zad 6
+
+#Zadanie 6.2.
 for i in range(len(plansza)):
     for j in range(len(plansza[0])):
         licznik = 0
         srodkowy = plansza[i][j] #szary element
-        '''if srodkowy > plansza [i - 1][j]:
+        '''if srodkowy < plansza[i - 1][j]:
             licznik += 1
-        if srodkowy < plansza [i + 1][j +1]:
+        if srodkowy < plansza[i + 1][j + 1]:
             licznik += 1'''
 
         for x in range(i - 1, (i + 1) + 1):
             for y in range(j - 1, (j + 1) + 1):
-                if x < 0:
+                if i < 0:
                     x = len(plansza) - 1
                 elif x > len(plansza) - 1:
                     x = 0
@@ -69,11 +71,4 @@ for i in range(len(plansza)):
                 czerwony = plansza[x][y]
                 if srodkowy < czerwony:
                     licznik += 1
-            print(i, j, licznik)
-
-
-
-
-
-
-
+        print(i, j, licznik)
