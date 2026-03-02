800ce3c01960a8ee18c8d015bafd0a0862de9e59
1C gr 2 <lukasz.luc.2020@gmail.com>
2026-03-02 11:25:21 +0100

    funkcje cd

--- funkcje_zadania.py
+++ funkcje_zadania.py
@@ -1,6 +1,6 @@
 #zad 0.3
 #a)
-def suma_v(u, v):
+'''def suma_v(u, v):
     w = []
     for i in range(len(u)):
         suma = u[i] + v[i]
@@ -22,3 +22,29 @@ u = [2, 7, 3]
 v = [-1, 0, 4]
 wynik = iloczyn_v(u, v)
 print(wynik)
+'''
+#zadanie 2.1
+def czy_anagramy(s1, s2):
+    '''if sorted(s1) == sorted(s2):
+        return True
+    else:
+        return False'''
+    return sorted(s1) == sorted(s2)
+#print(czy_anagramy('nosek', 'keson'))
+'''s1= 'nosek'
+s2 = 'keson'
+print(sorted(s1) == sorted(s2))
+'''
+
+#2.2
+def jaki_trojkat(a, b, c):
+    if a + b + c > 2 * max([a, b, c]) ** 2:
+        if a ** 2 + b ** 2 + c ** 2 == 2 * max([a, b, c]) ** 2:
+            print('prostokatny')
+        elif a ** 2 + b ** 2 + c ** 2 > 2 * max([a, b, c]) ** 2:
+            print('ostrokatny')
+        elif a ** 2 + b ** 2 + c ** 2 < 2 * max([a, b, c]) ** 2:
+            print('rozwartokatny')
+    else:
+        print('to niejest trojkat')
+jaki_trojkat(10, 5, 12)
